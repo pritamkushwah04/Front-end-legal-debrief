689fd78a5cc91c4e6d30cc641a8d896b94fa0b46
pritamkushwah04 <pritamkushwah.04.08@gmail.com>
2023-09-23 00:44:16 +0530

    icon added

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
+  // baseURL :"https://safe-beautiful-chevre.glitch.me/",
   baseURL: "https://backend-legal-debrief.onrender.com/api",
   
 });
